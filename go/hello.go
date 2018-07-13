9bc6744003d117d07d4ed01cc86d709981fdd634
developerdiary <35128421+geeknotes@users.noreply.github.com>
2018-07-12 21:49:43 -0700

    Create hello.go

---
+++ go/hello.go
@@ -0,0 +1,24 @@
+/*
+Prerequisite : Go is installed
+Go in the directory which has this file
+
+Execution
+go build
+./hello
+
+or 
+go run main.go 
+
+Output: 
+Welcome
+
+Format tool 
+gofmt -w main.go
+*/
+
+package main
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome")
+}
